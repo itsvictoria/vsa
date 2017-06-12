391580348c02e4630150deef3e6d60c5f6206035
Peabody <peabody@dhcp-129-59-214-88.n1.vanderbilt.edu>
2017-06-12 11:41:25 -0500

    proj1

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -7,4 +7,20 @@
 
 # If you complete extensions, describe your extensions here!
 
+name = raw_input("Enter your first name: ")
+name_inital = name[:1]
+inital_capital = name_inital.upper()
+name_rest = name[1:]
+rest_lowercase = name_rest.lower()
+
+age = raw_input("Enter your age: ")
+birthday = raw_input ("Have you had a birthday this year (yes or no)?: ")
+if birthday == "yes":
+  one_hundred = 100 - int(age) + 2017
+  print inital_capital + rest_lowercase + " will turn 100 in the year ",
+  print(one_hundred)
+else:
+  one_hundred = 100 - int(age) + 2016
+  print inital_capital + rest_lowercase + " will turn 100 in the year ",
+  print(one_hundred)
 
